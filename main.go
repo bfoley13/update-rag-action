cfeee05e3519815e57d8a8cbecccda04492f1cd8
Brandon Foley <brandonfoley13@gmail.com>
2025-05-12 10:59:54 -0400

    adding create/update

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"io/fs"
+	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	githubactions "github.com/sethvargo/go-githubactions"
 )
@@ -25,15 +28,33 @@ func main() {
 
 	githubactions.Infof("ragHost: %s | ragPort: %s | branch: %s", ragHost, ragPort, branch)
 	ragClient := NewRagClient(ragHost, ragPort, branch)
-	_, err := ragClient.CheckIfIndexExists()
+	indexExists, err := ragClient.CheckIfIndexExists()
 	if err != nil {
 		githubactions.Fatalf("failed to check if index exists: %v", err)
 	}
 
-	createIndex(nil)
+	if !indexExists {
+		githubactions.Infof("Index does not exist, creating index")
+		createIndex(ragClient)
+	} else {
+		githubactions.Infof("Index already exists, updating index")
+		updatedFiles, err := getUpdatedFiles()
+		if err != nil {
+			githubactions.Fatalf("failed to get updated files: %v", err)
+		}
+
+		if len(updatedFiles) == 0 {
+			githubactions.Infof("No updated files found")
+			return
+		}
+
+		updateIndex(ragClient, updatedFiles)
+		githubactions.Infof("Index updated successfully")
+	}
 }
 
 func createIndex(ragClient *RagClient) {
+	documents := []*RagDocument{}
 	err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
@@ -41,12 +62,77 @@ func createIndex(ragClient *RagClient) {
 		if d.IsDir() {
 			return nil
 		}
+		if !strings.HasSuffix(path, ".go") {
+			return nil
+		}
+
 		githubactions.Infof("Processing file: %s", path)
+
+		fileBytes, err := os.ReadFile(path)
+		if err != nil {
+			githubactions.Fatalf("failed to read file: %v", err)
+		}
+
+		fileContent := string(fileBytes)
+		documents = append(documents, &RagDocument{
+			Text:     fileContent,
+			Metadata: map[string]string{"file_path": path, "file_name": d.Name()},
+		})
+
 		return nil
 	})
+
 	if err != nil {
-		githubactions.Fatalf("failed to walk directory: %v", err)
+		githubactions.Fatalf("failed to create documents for index create: %v", err)
+	}
+
+	_, err = ragClient.CreateIndex(documents)
+	if err != nil {
+		githubactions.Fatalf("failed to create index: %v", err)
 	}
 
 	githubactions.Infof("Index created successfully")
+}
+
+func getUpdatedFiles() ([]string, error) {
+	cmd := exec.Command("git", "diff", "--name-only", "HEAD", "HEAD~1")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	githubactions.Infof("Updated files: %s", string(output))
+
+	files := strings.Split(string(output), "\n")
+	for i := range files {
+		files[i] = strings.TrimSpace(files[i])
+	}
+	return files, nil
+}
+
+func updateIndex(ragClient *RagClient, updatedFiles []string) {
+	documents := []*RagDocument{}
+	for _, file := range updatedFiles {
+		githubactions.Infof("Processing file: %s", file)
+
+		fileBytes, err := os.ReadFile(file)
+		if err != nil {
+			githubactions.Fatalf("failed to read file: %v", err)
+		}
+
+		fileContent := string(fileBytes)
+		documents = append(documents, &RagDocument{
+			Text:     fileContent,
+			Metadata: map[string]string{"file_path": file, "file_name": filepath.Base(file)},
+		})
+	}
+
+	updateResponse, err := ragClient.UpdateDocuments(documents)
+	if err != nil {
+		githubactions.Fatalf("failed to update index: %v", err)
+	}
+	githubactions.Infof("Index updated successfully")
+	githubactions.Infof("Updated documents: %v", updateResponse.UpdatedDocuments)
+	githubactions.Infof("Unchanged documents: %v", updateResponse.UnchangedDocuments)
+	githubactions.Infof("Not found documents: %v", updateResponse.NotFoundDocuments)
 }
